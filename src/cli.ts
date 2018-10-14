907d220e4ab3e78a362371c374aba09cce90458c
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-10-14 16:05:30 +0200

    Refactor sync command into own file

--- src/cli.ts
+++ src/cli.ts
@@ -1,15 +1,8 @@
 #!/usr/bin/env node
 import chalk from "chalk"
 import minimist from "minimist"
-import Nehemiah from "nehemiah"
-import path from "path"
 
-import { Action } from "./Action"
-import { EditorConfig } from "./actions/EditorConfig"
-import { GitFetchPrune } from "./actions/GitFetchPrune"
-import { License } from "./actions/License"
-import { PackageConfig } from "./actions/PackageConfig"
-import { TSLintConfig } from "./actions/TSLintConfig"
+import { sync } from "./commands/sync"
 
 function fail(message: string) {
   console.error(chalk.red(message))
@@ -18,44 +11,6 @@ function fail(message: string) {
 
 function parseArgs() {
   return minimist(process.argv.slice(2))
-}
-
-async function performAction(action: Action, projects: Nehemiah[]) {
-  console.log(chalk.bgYellow.black(action.name()))
-
-  await Promise.all(projects.map(async project => {
-    if (await action.applies(project)) {
-      await action.execute(project)
-      console.log("  " + path.basename(project.cwd))
-    }
-  }))
-}
-
-async function sync(root: string) {
-  if (!root) {
-    root = path.resolve(__dirname, "../..")
-    // fail("Please specify the root directory as first argument")
-  }
-
-  const n = new Nehemiah(root)
-  const options = { onlyDirectories: true, deep: 1 }
-  const dirs = await n.find("**/.git", options)
-  const projects = dirs
-    .map(dir => path.dirname(dir))
-    .map(dir => path.join(root, dir))
-    .map(dir => new Nehemiah(dir))
-
-  const actions: Action[] = [
-    new EditorConfig(),
-    new GitFetchPrune(),
-    new License(),
-    new PackageConfig(),
-    new TSLintConfig(),
-  ]
-
-  for (const action of actions) {
-    await performAction(action, projects)
-  }
 }
 
 async function main() {
---
+++ src/commands/sync.ts
@@ -0,0 +1,48 @@
+import chalk from "chalk"
+import Nehemiah from "nehemiah"
+import path from "path"
+
+import { Action } from "../Action"
+import { EditorConfig } from "../actions/EditorConfig"
+import { GitFetchPrune } from "../actions/GitFetchPrune"
+import { License } from "../actions/License"
+import { PackageConfig } from "../actions/PackageConfig"
+import { TSLintConfig } from "../actions/TSLintConfig"
+
+const actions: Action[] = [
+  new EditorConfig(),
+  new GitFetchPrune(),
+  new License(),
+  new PackageConfig(),
+  new TSLintConfig(),
+]
+
+async function performAction(action: Action, projects: Nehemiah[]) {
+  console.log(chalk.bgYellow.black(action.name()))
+
+  await Promise.all(projects.map(async project => {
+    if (await action.applies(project)) {
+      await action.execute(project)
+      console.log("  " + path.basename(project.cwd))
+    }
+  }))
+}
+
+export async function sync(rootDir: string) {
+  if (!rootDir) {
+    rootDir = path.resolve(__dirname, "../..")
+    // fail("Please specify the root directory as first argument")
+  }
+
+  const n = new Nehemiah(rootDir)
+  const options = { onlyDirectories: true, deep: 1 }
+  const dirs = await n.find("**/.git", options)
+  const projects = dirs
+    .map(dir => path.dirname(dir))
+    .map(dir => path.join(rootDir, dir))
+    .map(dir => new Nehemiah(dir))
+
+  for (const action of actions) {
+    await performAction(action, projects)
+  }
+}
