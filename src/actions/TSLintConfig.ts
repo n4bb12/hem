7cccdc79f4c3d34a8517b5239fb1ee6a040518af
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-10-14 16:07:17 +0200

    Print warning when TSLint settings differ from preset

--- src/actions/TSLintConfig.ts
+++ src/actions/TSLintConfig.ts
@@ -1,4 +1,4 @@
-import template from "@n4bb12/config-tslint"
+import preset from "@n4bb12/config-tslint"
 import cleanDeep from "clean-deep"
 import { isEqual, uniq } from "lodash"
 import Nehemiah from "nehemiah"
@@ -7,7 +7,13 @@ import { Action } from "../Action"
 
 const projectFile = "tsconfig.json"
 const configFile = "tslint.json"
-const preset = "@n4bb12/config-tslint"
+const presetName = "@n4bb12/config-tslint"
+const template = {
+  $schema: "http://json.schemastore.org/tslint",
+  extends: [
+    "@n4bb12/config-tslint",
+  ],
+}
 
 export class TSLintConfig implements Action {
 
@@ -26,20 +32,26 @@ export class TSLintConfig implements Action {
     const config = await n.read(configFile).asJson() || {}
 
     config.$schema = "http://json.schemastore.org/tslint"
-    config.extends = uniq([...config.extends || [], preset])
+    config.extends = uniq([...config.extends || [], presetName])
 
     if (config.rules) {
       Object.keys(config.rules).forEach(key => {
-        if (isEqual(template.rules[key], config.rules[key])) {
+        if (isEqual(preset.rules[key], config.rules[key])) {
           delete config.rules[key]
         }
       })
       config.rules = cleanDeep(config.rules)
     }
 
+    const cleanConfig = cleanDeep(config)
+
+    if (!isEqual(cleanConfig, template)) {
+      n.warn(n.name, "has custom TSLint settings")
+    }
+
     await Promise.all([
-      n.run("yarn add --dev @n4bb12/config-tslint"),
-      n.write(configFile).asJson(cleanDeep(config)),
+      n.run("yarn add --dev tslint @n4bb12/config-tslint"),
+      n.write(configFile).asJson(cleanConfig),
     ])
   }
 
